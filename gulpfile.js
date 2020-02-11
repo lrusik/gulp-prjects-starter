4bd6ec28eadb529dd7e121e0557ab2fc20c30d0c
Ruslan Yakushev <yakushev.rusl101@gmail.com>
2020-02-11 06:07:05 +0200

    init commit

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+var requireDir = require('require-dir');
+let proj_name = process.argv[3].substr(2);
+const gulp = require('gulp');
+let proj_args = "";
+
+module.exports = { sshConfig: {
+  host: '',
+  port: 22,
+  username: '',
+  password: ''
+} };
+
+if( typeof(process.argv[4]) != "undefined" ){
+	proj_args = "_" + process.argv[4].substr(2);
+}
+
+requireDir(proj_name);
+gulp.task("start", function() { 
+	return new Promise(function(resolve, reject) {
+		(gulp.parallel(proj_name + proj_args)());
+		resolve();
+	});
+});
